6e7efc813db47a8267a2004b357ac8d1661e89c7
PabloRuiz-TT <pablo.ruiz@trafictours.com>
2024-06-01 17:23:08 -0500

    Agregar archivos de proyecto.

---
+++ BaseWeb/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace BaseWeb
+{
+    public class Startup(IConfiguration configuration)
+    {
+        public IConfiguration Configuration { get; } = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+        }
+    }
+}
